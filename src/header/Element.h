07009cb4c05f22659a182a644b3dcfd8234c302a
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-13 15:20:57 -0400

    Update Element.h

--- src/header/Element.h
+++ src/header/Element.h
@@ -12,22 +12,22 @@
 #include <iostream>
 #include <string>
 
-
-class Element {
+class Element
+{
 public:
   // element names
-    static constexpr const char* const ELE_WATER = "Water";
-    static constexpr const char* const ELE_WOOD = "Wood";
-    static constexpr const char* const ELE_FIRE = "Fire";
-    static constexpr const char* const ELE_EARTH = "Earth";
-    static constexpr const char* const ELE_METAL = "Metal";
+  static constexpr const char *const ELE_WATER = "Water";
+  static constexpr const char *const ELE_WOOD = "Wood";
+  static constexpr const char *const ELE_FIRE = "Fire";
+  static constexpr const char *const ELE_EARTH = "Earth";
+  static constexpr const char *const ELE_METAL = "Metal";
 
   // EFFECTS Initializes Element to Water
   Element();
 
   // REQUIRES element is "Water", "Wood", "Fire", "Earth", or "Metal"
   // EFFECTS Initializes Element to specified element.
-  Element(const std::string& element);
+  Element(const std::string &element);
 
   // EFFECTS Returns the element
   std::string get_element() const;
@@ -40,46 +40,46 @@ public:
 
   // REQUIRES element is "Water", "Wood", "Fire", "Earth", or "Metal"
   // EFFECTS Changes element type to specified element.
-  void change_element(const std::string& element);
+  void change_element(const std::string &element);
 
   // REQUIRES resource is a valid element
-  // EFFECTS Returns true if other resource element strengthens 
-  //or harms current element. False otherwise.
-  bool is_special_resource(const Element& resource) const;
+  // EFFECTS Returns true if other resource element strengthens
+  // or harms current element. False otherwise.
+  bool is_special_resource(const Element &resource) const;
 
   // REQUIRES resource either strengthens or harms current element
   // EFFECTS Returns true if other resource element strengthens current element. False if harms.
-  bool is_strengthening(const Element& resource) const;
+  bool is_strengthening(const Element &resource) const;
 
   // REQUIRES defender is a valid element
-  // EFFECTS Returns true if current element is extremely effective or 
-  //extremely ineffective against defender element. False otherwise.
-  bool is_special_strength(const Element& defender) const;
+  // EFFECTS Returns true if current element is extremely effective or
+  // extremely ineffective against defender element. False otherwise.
+  bool is_special_strength(const Element &defender) const;
 
   // REQUIRES current element is extremely effective or extremely ineffective against defender element.
   // EFFECTS Returns true if current element is extremely effective
-  //against defender element. False otherwise.
-  bool is_effective_strength(const Element& defender) const;
+  // against defender element. False otherwise.
+  bool is_effective_strength(const Element &defender) const;
 
 private:
-    std::string element;
-    std::string best_healing_element; // element that current element would like to receive healing from
-    std::string best_opponent_element; // element that current element would like to have as opponent
+  std::string element;
+  std::string best_healing_element;  // element that current element would like to receive healing from
+  std::string best_opponent_element; // element that current element would like to have as opponent
 };
 
 // EFFECTS Returns the element that current element would like to receive healing from
-std::string find_best_healing_element(const std::string& element);
+std::string find_best_healing_element(const std::string &element);
 
 // EFFECTS Returns the element that current element would like to fight as opponent
-std::string find_best_opponent_element(const std::string& element);
+std::string find_best_opponent_element(const std::string &element);
 
 // EFFECTS Returns true if lhs is same as rhs
-bool operator==(const Element& lhs, const Element& rhs);
+bool operator==(const Element &lhs, const Element &rhs);
 
 // EFFECTS Returns true if lhs is not same as rhs.
-bool operator!=(const Element& lhs, const Element& rhs);
+bool operator!=(const Element &lhs, const Element &rhs);
 
 // EFFECTS Prints Element to stream, for example "Water"
-std::ostream & operator<<(std::ostream& os, const Element& element);
+std::ostream &operator<<(std::ostream &os, const Element &element);
 
 #endif
